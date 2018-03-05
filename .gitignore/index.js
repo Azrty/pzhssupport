0cacb0eb8c047b1d25a8e6f43685c14b8baa1d70
MhBurgerKing <34395040+MhBurgerKing@users.noreply.github.com>
2018-03-05 18:16:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,38 +16,35 @@ bot.on('ready', function() {
 });
 
 
-bot.login(process.env.TOKEN);
-
+bot.login("NDE5ODYzOTAwNDQ4NzUxNjI3.DX7mtw.vDbTmIe-rB-IvVp_zTaeqtFEcwo");
 
 bot.on('message', message => {
 
-
     var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else{
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb);
-        var userxp = Object.values(userxpdb)
-        console.log(userxp)
-        console.log(`Nombre d'xp : ${userxp[1]}`)
-
-        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-
-    if (message.content === prefix + "xp"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Salut ! voici tes statistiques, ${message.author.username} !`)
-            .setColor('#009933')
-            .setDescription("Si tu es actif, tu peux réclamer le rank Membre Confirmé !")
-            .addField("Tes xp:", `${xpfinal[1]} xp`)
-            .setFooter("Bon moment parmis la PZH's Community")
-        message.channel.send({embed: xp_embed});
-        
+    
+        if(message.author.bot)return;
+    
+        if(!db.get("xp").find({user: msgauthor}).value()){
+            db.get("xp").push({user: msgauthor, xp: 1}).write();
+        }else{
+            var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+            console.log(userxpdb);
+            var userxp = Object.values(userxpdb)
+            console.log(userxp)
+            console.log(`Nombre d'xp : ${userxp[1]}`)
+    
+            db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+    
+        if (message.content === prefix + "xp"){
+            var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+            var xpfinal = Object.values(xp);
+            var xp_embed = new Discord.RichEmbed()
+                .setTitle(`Salut ! voici tes statistiques, ${message.author.username} !`)
+                .setColor('#009933')
+                .setDescription("Si tu es actif, tu peux réclamer le rank Membre Confirmé !")
+                .addField("Tes xp:", `${xpfinal[1]} xp`)
+                .setFooter("Bon moment parmis la PZH's Community")
+            message.channel.send({embed: xp_embed});
     }
 
     if (message.content === prefix + "support"){
@@ -71,22 +68,13 @@ bot.on('message', message => {
             .setDescription("Information du bot")
             .addField("Création", "Le 10/01/2018 à 17h17", true)
             .addField("Créateur", "Crée par [PZH](https://www.youtube.com/c/pzhcodage)", true)
-            .addField("Version du bot", "Version 3.0", true)
-            .addField("Dernière mise à jour:", "Le 04/03/2018 à 14:00", true)
+            .addField("Version du bot", "Version 4.0", true)
+            .addField("Dernière mise à jour:", "Le 05/03/2018 à 18:02", true)
             .addField("à savoir", "Si vous cliquez sur [PZH](https://www.youtube.com/c/pzhcodage), cela vous redirigera sur sa chaîne YouTube", true)
             .setColor("0xE74C3C")
             .addField("Apprend à coder un bot discord !", "Suivez les tuto de [PZH](https://www.youtube.com/c/pzhcodage) sur sa chaîne youtube !", true)
             .setFooter("Toute ressemblance avec un autre bot serait qu'une pure coïncidence. Bon moment parmis la PZH's Community")
         message.channel.sendEmbed(embed);
-    }
-
-    if (message.content.startsWith(prefix + "oodere5ntr")) {
-        message.delete();
-        let args = message.content.split(" ").slice(1);
-        let thingToEcho = args.join(" ")
-        message.guild.channels.find("name", "general").sendMessage(thingToEcho)
-        message.channel.sendMessage(thingToEcho)
-        message.channel.sendMessage(`${message.author.username}, le message ci-dessus a bien été envoyé dans #general`)
     }
 
     if (message.content === prefix + "help"){
@@ -102,15 +90,62 @@ bot.on('message', message => {
         .setFooter("Bon moment parmis la PZH's Community")
     message.author.sendEmbed(embed)
     message.channel.sendMessage(`${message.author.username}, la page d'aide a été envoyé en message privée :thumbsup:`)
-        
-    }
 
-    if (message.content.startsWith(prefix + "oodere5ntrmsg")) {
-        message.delete();
+    }
+    
+    if (message.content.startsWith(prefix + "say")) {
+        let modRole = message.guild.roles.find("name", "Modérateurs");
+        if(message.member.roles.has(modRole.id)) {
         let args = message.content.split(" ").slice(1);
         let thingToEcho = args.join(" ")
-        message.author.sendMessage(thingToEcho)
+        message.channel.sendMessage(thingToEcho)
+    } else {
+        message.reply(`tu n'as pas la permission de faire cette commande.`)
+            
 
+bot.on('message', message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.length).split(/ +/);
+    command = args.shift().toLowerCase();
+
+
+    if (command === "kick") {
+        let modRole = message.guild.roles.find("name", "Modérateurs");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Merci de mentionner l'utilisateur à expulser.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+            return message.reply("Cet utilisateur est introuvable ou impossible à expulser.");
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(member => {
+            message.reply(`${member.user.username} a été expulsé avec succès.`).catch(console.error);
+        }).catch(console.error)
+    
+    }
+
+    if (command === "ban") {
+        let modRole = message.guild.roles.find("name", "Modérateurs");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+        }
+        const member = message.mentions.members.first();
+        if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
+        member.ban().then(member => {
+            message.reply(`${member.user.username} a été banni avec succès.`).catch(console.error);
+        member.ban({
+            days: args[1] || null,
+            reason: `Vous avez été banni`
+        })
+
+    
+        
 
 
 bot.on('guildMemberAdd', member => {
@@ -122,4 +157,4 @@ bot.on("guildMemberAdd", member => {
     
 bot.on("guildMemberRemove", member =>{
     member.guild.channels.find("name", "general").send(`${member.user.username} a quitté le discord de la **PZH's Community**... à bientôt :(`)
-})})})}}})
+})})})})}})}}}})
