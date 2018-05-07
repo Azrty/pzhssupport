447781a8c776bcfab358f9dc1a306cd181ed5687
MhBurgerKing <34395040+MhBurgerKing@users.noreply.github.com>
2018-05-07 16:34:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -180,7 +180,7 @@ bot.on("message", function(message) {
                 .addField("Crée le", message.guild.createdAt)
                 .addField("Tu as rejoin le", message.member.joinedAt)
                 .addField("Utilisateurs sur le discord", message.guild.memberCount)
-                .addField("Test", "test <#" + 405705028783964170 + ">")
+                .addField("Test", "test <# + channel.id + >)
                 .setColor("0x0000FF")
             message.channel.sendEmbed(embed)
             break;
