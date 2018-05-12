d122ca0d45b3b3d483be0536cac9ba9d83a4d1e8
MhBurgerKing <34395040+MhBurgerKing@users.noreply.github.com>
2018-05-12 15:23:36 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -336,4 +336,28 @@ bot.on('message', message => {
                 if (role) return message.reply("Une erreur est survenue ! Réssayer plus tard.")
     }})
 
+bot.on("message", message => {
+
+    if(message.content === prefix + "nsfw") {
+        message.channel.send("Cet accès peut choquer ou toucher la sensibilité des plus jeunes \nVous êtes responsable de ce que vous allez voir.\nëtes vous sûr d'avoir accès au NSFW ?").then(mess => {
+            mess.react('✅')
+            mess.react('❌')
+            const track = mess.createReactionCollector((r, u) => u.id === message.author.id)
+            track.on('collect', r => {
+                if (r.emoji.name === "✅") { 
+                message.channel.bulkDelete(1)
+                    var role = message.guild.roles.find('name', '⚠️ NSFW');
+                    message.member.addRole(role)
+                    message.reply("Vous avez bien reçu l'accès au NSFW")}
+                if (r.emoji.name === "❌") {
+                    message.channel.bulkDelete(1)}
+
+
+                    
+                }
+            
+        
+    
+)})}})
+
 
